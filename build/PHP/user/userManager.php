5d5666658f929a58514c065d1ed600f36b52ddbb
FrankZn <lygzzy@gmail.com>
2018-06-02 20:14:54 +0800

    Add getUsername and checkPassword for UserManager.

--- build/PHP/user/userManager.php
+++ build/PHP/user/userManager.php
@@ -59,6 +59,32 @@ class UserManager {
 	function getId() {
 		return $_SESSION['user_id'];
 	}
+	function getUsername() {
+		return $_SESSION['username'];
+	}
+	/**
+	 * @param string $pwd 密码
+	 * @param string $username 用户名，如果不提供则使用_SESSION['username']
+	 * @return bool 用户名不存在或密码错误返回FALSE
+	 */
+	function checkPassword($pwd, $username = NULL) {
+		if($username == NULL)
+			$username = $this->getUsername();
+
+		$query = "SELECT password FROM users WHERE username= ?";
+		$stmt = $this->mysql->prepare($query);
+		$stmt->bind_param('s',$username);
+		$stmt->execute();
+		$stmt->bind_result($mysql_pwd);
+
+		if($stmt->fetch()) {
+			if($pwd != $mysql_pwd) {
+				return FALSE;
+			}
+		}
+		// Username does not exist or password is wrong.
+		return FALSE;
+	}
 }
 session_start();
 $_UserManager = new UserManager();
